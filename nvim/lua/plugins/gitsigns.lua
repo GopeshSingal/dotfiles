ca6449f6491ec7a29c36c93b76c71472d137dbc4
GopeshSingal <gopesh.singal.03@gmail.com>
2026-09-12 23:50:14 -0400

    feat: make gitsigns easier to see

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -10,11 +10,14 @@ gs.setup({
         untracked    = { text = "○" },
     },
     signcolumn = true,
+    numhl = true,
 })
 
-vim.api.nvim_set_hl(0, "GitSignsAdd",    { bg = "none", fg = "#a3be8c" })
-vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "none", fg = "#ebcb8b" })
-vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none", fg = "#bf616a" })
+vim.api.nvim_set_hl(0, "GitSignsAdd",          { bg = "none", fg = "#a3be8c" })
+vim.api.nvim_set_hl(0, "GitSignsChange",       { bg = "none", fg = "#ebcb8b" })
+vim.api.nvim_set_hl(0, "GitSignsDelete",       { bg = "none", fg = "#e27878" })
+vim.api.nvim_set_hl(0, "GitSignsUntracked",    { bg = "none", fg = "#88c0d0" })
+vim.api.nvim_set_hl(0, "GitSignsChangedelete", { bg = "none", fg = "#d08770" })
 
 vim.keymap.set("n", "]h",         function() gs.nav_hunk('next') end, { desc = "Next git hunk" })
 vim.keymap.set("n", "[h",         function() gs.nav_hunk('prev') end, { desc = "Previous git hunk" })
